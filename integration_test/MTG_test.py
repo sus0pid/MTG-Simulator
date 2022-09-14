8f860f6d1c2280a2de7eece7c6c848cce979b929
sus0pid <s2136500@ed.ac.uk>
2022-09-14 09:52:34 +0100

    rename the test python script

--- integration_test/MTG_test.py
+++ integration_test/MTG_test.py
@@ -5,11 +5,11 @@ from static_simulator import *
 # sys.path.append("../utilities")
 # sys.path.append("../constructions")
 
-# dynamic_simulation("bwrand")
-# static_simulation("bwrand")
+dynamic_simulation("bwrand")
+static_simulation("bwrand")
 
 dynamic_simulation("bowtie")
-static_simulation("bowtie")
+static_simulation("bowtie") # TODO: a bug in ouput file: two extra 0s in bad_bw_l0 & bad_bw_l1
 
 dynamic_simulation("randrand")
 static_simulation("randrand")
